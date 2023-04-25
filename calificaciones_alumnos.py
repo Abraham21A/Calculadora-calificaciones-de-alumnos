dbb55f83b0252b5731ed88810d478e6aa455334c
Abraham Alatorre <kofi4028@gmail.com>
2023-04-24 22:48:52 -0600

    Cambios-Actualización de código

--- calificaciones_alumnos.py
+++ calificaciones_alumnos.py
@@ -1,70 +1,63 @@
-# Programa de promedio de calificaciones de alumnos
+# Se crea una lista vacía llamada lista_calificaciones que servirá 
+# para almacenar los nombres de los alumnos y sus respectivas calificaciones.
+lista_calificaciones = []
 
-# Crear una lista vacía para almacenar la información de los alumnos
-lista = []
-# Inicializar un contador para llevar el registro de la cantidad de alumnos ingresados
-alumnos = 0
+print("""
+Agregar un nuevo alumno (1)
+Ver los alumnos y las calificaciones (2)
+Salir (S)
+""")
 
-# Iniciar un ciclo while infinito para que el programa siga ejecutándose hasta que el usuario decida finalizarlo
 while True:
-    # Solicitar al usuario que ingrese una opción: 1 para ingresar usuarios, 2 para finalizar el programa
-    opciones = input("Ingrese (1) para ingresar usuarios, (2) para finalizar programa: ")
-
-    # Si el usuario ingresó '1', continuar con el proceso de ingreso de usuarios
-    if opciones == '1':
-        # Solicitar al usuario que ingrese la cantidad de alumnos que desea ingresar
-        numero_alumnos = input("Ingrese la cantidad de alumnos que desea ingresar: ")
-
-        # Verificar si el valor ingresado es numérico
-        if numero_alumnos.isnumeric():
-            # Convertir el valor ingresado a un entero
-            numero_alumnos = int(numero_alumnos)
-            # Iniciar un ciclo for para iterar por cada uno de los alumnos que el usuario desea ingresar
-            for numero_alumno in range(1, numero_alumnos+1):
-                # Iniciar un ciclo while para validar que se ingresen los datos correctamente
-                while alumnos < numero_alumno:
-                    # Solicitar al usuario que ingrese el nombre del alumno y validar que solo contenga letras
-                    nombre_alumno = input("Ingrese el nombre del alumno: ").capitalize()
-                    if nombre_alumno.isnumeric() or nombre_alumno == "":
-                        print("El nombre del alumno no puede estar vacío y debe contener solo letras")
-                        continue
-                    else:
-                        # Solicitar al usuario que ingrese la primera calificación del alumno y validar que sea numérica
-                        primera_calificacion = input(f"Ingrese la primera calificacion de {nombre_alumno}: ")
-                        if primera_calificacion.isnumeric():
-                            try:
-                                # Convertir la calificación ingresada a un entero
-                                primera_calificacion = int(primera_calificacion)
-                                # Solicitar al usuario que ingrese la segunda y tercera calificación del alumno y convertirla a un entero
-                                segunda_calificacion = int(input(f"Ingrese la segunda calificacion de {nombre_alumno}: "))
-                                tercera_calificacion = int(input(f"Ingrese la tercera calificacion de {nombre_alumno}: "))
-                                # Crear una lista con el nombre y las tres calificaciones del alumno y agregarla a la lista general de alumnos
-                                lista_alumno = [nombre_alumno, primera_calificacion, segunda_calificacion, tercera_calificacion]
-                                lista.append(lista_alumno)
-                                # Incrementar el contador de alumnos ingresados
-                                alumnos += 1
-
-                            except ValueError:
-                                print("Calificación no válida, ingrese una calificación correcta")
-                                continue 
-                        else:
-                            print("Calificación no válida, debe haber una calificación")
-                            continue
+    menu = input("Ingresa una opcion: ")
+    
+    # Si el usuario no ingresa ninguna opción, se muestra un mensaje de error.
+    if menu == "":
+        print("La opcion esta vacia, ingresa una opcion valida")
+        
+    # Si el usuario selecciona la opción "1", 
+		# se le solicita que ingrese el nombre del alumno y el número de calificaciones que desea ingresar.
+    elif menu == "1":
+        nombre_alumno = input("Ingresa el nombre del alumno: ")
+        if nombre_alumno == "":
+            print("Dato vacio, ingrese un nombre")
         else:
-            print("Vuelva a ingresar el numero de alumnos, Dato no valido")
-            continue
-    # Si el usuario ingresó '2', finalizar el programa
-    elif opciones == "2":
-        print("El programa ha finalizado")
-        exit()
-    # Si el usuario ingresa una opción inválida, se muestra un mensaje de error y se continua con el loop
+            numero_calificaciones = input("Cuantas calificaciones desea ingresar: ")
+            
+            # Si el usuario ingresa un número válido de calificaciones, se utiliza un bucle "for" para solicitar 
+						# cada calificación y agregarla a la lista de calificaciones del alumno.
+            if numero_calificaciones.isdigit():
+                calificaciones_alumno = []
+                for i in range(int(numero_calificaciones)):
+                    try:
+                        calificacion = int(input("Ingrese la calificacion del alumno: "))
+                        calificaciones_alumno.append(calificacion)
+                    except ValueError:
+                        print("Dato incorrecto, ingrese de nuevo una calificacion")
+                        
+                # Se agrega una lista con el nombre del alumno y sus calificaciones a la lista general de calificaciones.
+                lista_calificaciones.append([nombre_alumno, calificaciones_alumno])
+            else:
+                print("Dato incorrecto, ingrese un numero entero para las calificaciones")
+                
+    # Si el usuario selecciona la opción "2", se verifica si hay algún alumno registrado 
+		# y se muestra el nombre de cada alumno junto con su promedio de calificaciones.
+    elif menu == "2":
+        if lista_calificaciones:
+            for alumno in lista_calificaciones:
+                nombre_alumno = alumno[0]
+                calificaciones_alumno = alumno[1]
+                promedio = sum(calificaciones_alumno)/len(calificaciones_alumno)
+                print(f"{nombre_alumno} : promedio {promedio}")
+        else:
+            print("No hay alumnos registrados")
+            
+    # Si el usuario selecciona la opción "S" (o "s"), se rompe el bucle y se sale del programa.
+    elif menu.lower() == "s":
+        break
+    
+    # Si el usuario ingresa una opción inválida, se muestra un mensaje de error.
     else:
-        print("Valor invalido, ingrese una opción válida")
-        continue
-    # Loop para calcular y mostrar el promedio de cada alumno
-    for lista_alumnos in lista:
-        nombre = lista_alumnos[0]
-        calificaciones = lista_alumnos[1:]
-        promedio = sum(calificaciones) / len(calificaciones)
-        print(f"la calificacion promedio de {nombre} es: {promedio}.")
+        print("Dato incorrecto, ingrese una opcion valida")
+
 
